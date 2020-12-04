99ebaad823ddaf3099cdb8dad519a88b7824e70a
gdever <alexey@dodonov.pro>
2020-12-04 18:58:38 +0300

    feature/Some refactoringd

--- Mezon/Transport/Tests/RequestParamsUnitTest.php
+++ Mezon/Transport/Tests/RequestParamsUnitTest.php
@@ -3,39 +3,6 @@ namespace Mezon\Transport\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Mezon\Router\Router;
-use Mezon\Transport\RequestParamsInterface;
-
-class ConcreteFetcher extends \Mezon\Transport\RequestParams
-{
-    //TODO use MockParamsFetcher instead
-
-    public function __construct()
-    {
-        $router = new Router();
-
-        parent::__construct($router);
-    }
-
-    /**
-     * 
-     * {@inheritDoc}
-     * @see \Mezon\Transport\RequestParamsInterface::getParam()
-     */
-    public function getParam($param, $default = false)
-    {
-        return 1;
-    }
-    
-    /**
-     *
-     * {@inheritDoc}
-     * @see RequestParamsInterface::wasSubmitted()
-     */
-    public function wasSubmitted(string $param): bool
-    {
-        return $this->getParam($param, false) !== false;
-    }
-}
 
 /**
  *
@@ -49,7 +16,7 @@ class RequestParamsUnitTest extends TestCase
      */
     public function testConstructor(): void
     {
-        $fetcher = new ConcreteFetcher();
+        $fetcher = new MockParamsFetcher();
 
         $this->assertEquals(1, $fetcher->getParam('some-param'));
         $this->assertInstanceOf(Router::class, $fetcher->getRouter());
